e4c203b08be19ddff654213b896c671ffd647627
TaufiqTab <taufiqtab77@gmail.com>
2021-02-25 10:01:49 +0700

    golang init

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/getuser", returnAllUsers).Methods("GET")
+	http.Handle("/", router)
+	fmt.Println("Connected to port 1234")
+	log.Fatal(http.ListenAndServe(":1234", router))
+
+}
+
+func returnAllUsers(w http.ResponseWriter, r *http.Request) {
+	var users Users
+	var arr_user []Users
+	var response Response
+
+	db := connect()
+	defer db.Close()
+
+	rows, err := db.Query("Select id,first_name,last_name from person")
+	if err != nil {
+		log.Print(err)
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(&users.Id, &users.FirstName, &users.LastName); err != nil {
+			log.Fatal(err.Error())
+
+		} else {
+			arr_user = append(arr_user, users)
+		}
+	}
+
+	response.Status = 1
+	response.Message = "Success"
+	response.Data = arr_user
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+
+}
